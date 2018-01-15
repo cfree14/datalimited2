5df89701bded717b09808806ed34103b82a99207
cfree14 <cfree14@gmail.com>
2018-01-15 20:51:58 +0100

    added package testing script

---
+++ data-raw/test_package.R
@@ -0,0 +1,32 @@
+
+# Install package
+devtools::install_github("cfree14/datalimited2")
+library("datalimited2")
+
+# Test rORCS
+scores <- c(1, 2, NA, 2, 2, 3, 1.93, 2, 1, 2, 1, 3)
+rorcs(scores)
+
+# Test zBRT
+output <- zbrt(year=TIGERFLAT$yr, catch=TIGERFLAT$catch)
+plot_zbrt(output)
+
+# Test OCOM
+output <- ocom(year=TIGERFLAT$yr, catch=TIGERFLAT$catch, m=0.27)
+plot_ocom(output)
+
+# Test cMSY
+output <- cmsy2(year=SOLIRIS$yr, catch=SOLIRIS$ct, r.low=0.18, r.hi=1.02)
+plot_cmsy2(output)
+plot_cmsy2_mgmt(output)
+
+# Test BSM
+output <- bsm(year=SOLIRIS$yr, catch=SOLIRIS$ct, biomass=SOLIRIS$bt, btype="CPUE", r.low=0.18, r.hi=1.02)
+plot_cmsy2(output)
+plot_cmsy2_mgmt(output)
+
+
+
+
+
+
