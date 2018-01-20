39c020dd8b7541dedd7ddab6c403739d2917a702
cfree14 <cfree14@gmail.com>
2018-01-20 09:52:54 +0100

    rearranged testing order

--- data-raw/test_package.R
+++ data-raw/test_package.R
@@ -3,6 +3,11 @@
 devtools::install_github("cfree14/datalimited2")
 library("datalimited2")
 
+# Test BSM
+output <- bsm(year=SOLIRIS$yr, catch=SOLIRIS$ct, biomass=SOLIRIS$bt, btype="CPUE", r.low=0.18, r.hi=1.02)
+plot_cmsy2(output)
+plot_cmsy2_mgmt(output)
+
 # Test rORCS
 scores <- c(1, 2, NA, 2, 2, 3, 1.93, 2, 1, 2, 1, 3)
 rorcs(scores)
@@ -20,10 +25,6 @@ output <- cmsy2(year=SOLIRIS$yr, catch=SOLIRIS$ct, r.low=0.18, r.hi=1.02)
 plot_cmsy2(output)
 plot_cmsy2_mgmt(output)
 
-# Test BSM
-output <- bsm(year=SOLIRIS$yr, catch=SOLIRIS$ct, biomass=SOLIRIS$bt, btype="CPUE", r.low=0.18, r.hi=1.02)
-plot_cmsy2(output)
-plot_cmsy2_mgmt(output)
 
 
 
