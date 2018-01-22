3bf1fbe4d9fb64ca405cc834de7ccf44ad049072
cfree14 <cfree14@gmail.com>
2018-01-22 11:49:12 -0500

    changed to plot_dlm()

--- data-raw/test_package.R
+++ data-raw/test_package.R
@@ -1,12 +1,11 @@
 
 # Install package
 devtools::install_github("cfree14/datalimited2")
-library("datalimited2")
+library(datalimited2)
 
 # Test BSM
 output <- bsm(year=SOLIRIS$yr, catch=SOLIRIS$ct, biomass=SOLIRIS$bt, btype="CPUE", r.low=0.18, r.hi=1.02)
-plot_cmsy2(output)
-plot_cmsy2_mgmt(output)
+plot_dlm(output)
 
 # Test rORCS
 scores <- c(1, 2, NA, 2, 2, 3, 1.93, 2, 1, 2, 1, 3)
@@ -14,16 +13,15 @@ rorcs(scores)
 
 # Test zBRT
 output <- zbrt(year=TIGERFLAT$yr, catch=TIGERFLAT$catch)
-plot_zbrt(output)
+plot_dlm(output)
 
 # Test OCOM
 output <- ocom(year=TIGERFLAT$yr, catch=TIGERFLAT$catch, m=0.27)
-plot_ocom(output)
+plot_dlm(output)
 
 # Test cMSY
 output <- cmsy2(year=SOLIRIS$yr, catch=SOLIRIS$ct, r.low=0.18, r.hi=1.02)
-plot_cmsy2(output)
-plot_cmsy2_mgmt(output)
+plot_dlm(output)
 
 
 
