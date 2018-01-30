89a4ab88b24ba12a4e9e4bb42005cd13594dfb4b
cfree14 <cfree14@gmail.com>
2018-01-30 08:58:39 -0500

    got rid of saturation plot, added f/fmsy plot and kobe plot, works with new outputs

--- R/plot_ocom.R
+++ R/plot_ocom.R
@@ -3,17 +3,13 @@
 plot_ocom <- function(output){
 
   # Unpack output
-  b_ts <- output[["b_ts"]]
-  b_trajs <- output[["b_trajs"]]
-  s_ts <- output[["s_ts"]]
-  s_trajs <- output[["s_trajs"]]
-  bbmsy_ts <- output[["bbmsy_ts"]]
-  bbmsy_trajs <- output[["bbmsy_trajs"]]
-  krms <- output[["krms"]]
+  ref_ts <- output[["ref_ts"]]
+  ref_pts <- output[["ref_pts"]]
   krms_draws <- output[["krms_draws"]]
-  yr_final <- max(bbmsy_ts$year)
-  bbmsy_final <- bbmsy_ts$q0.5[nrow(bbmsy_ts)]
-  s_final <- s_ts$q0.5[nrow(s_ts)]
+  yr1 <- min(ref_ts$year)
+  yr2 <- max(ref_ts$year)
+  bbmsy_final <- ref_ts$bbmsy[nrow(ref_ts)]
+  ffmsy_final <- ref_ts$ffmsy[nrow(ref_ts)]
 
   # Plot settings
   par(mfrow=c(2,3), xpd=NA)
@@ -22,19 +18,19 @@ plot_ocom <- function(output){
   #################################
 
   # Plot catch
-  xmin <- floor(min(bbmsy_ts$year) / 10) * 10
-  xmax <- ceiling(max(bbmsy_ts$year) / 10) * 10
-  plot(catch ~ year, bbmsy_ts, bty="n", las=1, type="n",
+  xmin <- floor(min(ref_ts$year) / 10) * 10
+  xmax <- ceiling(max(ref_ts$year) / 10) * 10
+  plot(catch ~ year, ref_ts, bty="n", las=1, type="n",
        xlim=c(xmin, xmax), xlab="", ylab="Catch", main="A. Catch")
   # Add MSY shading
   rect(xleft=xmin, xright=xmax,
-       ybottom=krms$q0.025[krms$param=="msy"],
-       ytop=krms$q0.975[krms$param=="msy"], col="grey80", border=F)
+       ybottom=ref_pts$q0.025[ref_pts$param=="msy"],
+       ytop=ref_pts$q0.975[ref_pts$param=="msy"], col="grey80", border=F)
   # Add catch
-  lines(bbmsy_ts$year, bbmsy_ts$catch, lwd=1.1)
+  lines(ref_ts$year, ref_ts$catch, lwd=1.1)
   # Add MSY line and stats
-  lines(x=c(xmin, xmax), y=rep(krms$q0.5[krms$param=="msy"], 2), lty=2)
-  text(x=xmax-5, y=krms$q0.5[krms$param=="msy"], pos=c(3), labels="MSY", font=2)
+  lines(x=c(xmin, xmax), y=rep(ref_pts$q0.5[ref_pts$param=="msy"], 2), lty=2)
+  text(x=xmax-5, y=ref_pts$q0.5[ref_pts$param=="msy"], pos=c(3), labels="MSY", font=2)
 
   # B. Viable and best r/k pairs
   ##############################################################################
@@ -44,96 +40,72 @@ plot_ocom <- function(output){
        xlab="K", ylab='r', main="B. Viable r-k pairs")
 
   # Add best r-k pair
-  r <- krms$q0.5[krms$param=="r"]
-  r_hi <- krms$q0.975[krms$param=="r"]
-  r_lo <- krms$q0.025[krms$param=="r"]
-  k <- krms$q0.5[krms$param=="k"]
-  k_hi <- krms$q0.975[krms$param=="k"]
-  k_lo <- krms$q0.025[krms$param=="k"]
+  r <- ref_pts$q0.5[ref_pts$param=="r"]
+  r_hi <- ref_pts$q0.975[ref_pts$param=="r"]
+  r_lo <- ref_pts$q0.025[ref_pts$param=="r"]
+  k <- ref_pts$q0.5[ref_pts$param=="k"]
+  k_hi <- ref_pts$q0.975[ref_pts$param=="k"]
+  k_lo <- ref_pts$q0.025[ref_pts$param=="k"]
   points(k, r, pch=16, cex=1.3)
   lines(x=c(k_lo, k_hi), y=c(r,r))
   lines(x=c(k, k), y=c(r_lo,r_hi))
 
-  # Parameter estimates
-  ##############################################################################
 
-  # # Parameter estimate histograms
-  # params <- c("r", "k", "msy", "s")
-  # param_names <- c("r", "K", "MSY", "Saturation (B/K)")
-  # for(i in 1:length(params)){
-  #   vals <- krms_draws[,params[i]]
-  #   median(vals)
-  #   hist(vals, las=1, xlab=param_names[i], main=paste0("OCOM: ", param_names[i], " posterior"), col="grey80", border=F)
-  #   abline(v=median(vals), lwd=1.5, lty=2)
-  # }
-
-  # Empty plot
-  plot.new()
-
-  # C. Biomass time series
+  # C. B/BMSY time series
   ##############################################################################
 
   # Setup empty plot
-  b_trajs1 <- b_trajs / 1000
-  xmin <- floor(min(b_ts$year) / 10) * 10
-  xmax <- ceiling(max(b_ts$year) / 10) * 10
-  ymax <- ceiling(max(b_trajs1) / 10) * 10
-  plot(q0.5 ~ year, b_ts, type="n", bty="n", las=1,
-       xlim=c(xmin, xmax), ylim=c(0, ymax), xlab="", ylab="Biomass (in 1000s)",
-       main="C. Biomass")
-  # Add randomly selected trajectories
-  # for(i in 1:ncol(b_trajs1)){lines(x=b_ts$year, y=b_trajs1[,i], col="grey80")}
-  polygon(x=c(b_ts$year, rev(b_ts$year)),
-          y=c(b_ts$q0.025/1000, rev(b_ts$q0.975/1000)), col="grey80", border=F)
-  # Add median and 95% CI trajectories
-  lines(x=b_ts$year, y=b_ts$q0.5/1000, lwd=1.1)
-  # lines(x=b_ts$year, y=b_ts$q0.025/1000, lwd=1.1, lty=2)
-  # lines(x=b_ts$year, y=b_ts$q0.975/1000, lwd=1.1, lty=2)
-
-  # D. Saturation time series
-  ##############################################################################
-
-  # Setup empty plot
-  xmin <- floor(min(s_ts$year) / 10) * 10
-  xmax <- ceiling(max(s_ts$year) / 10) * 10
-  plot(q0.5 ~ year, s_ts, type="n", bty="n", las=1,
-       xlim=c(xmin, xmax), ylim=c(0,1), xlab="", ylab="Saturation",
-       main="D. Saturation (B/K)")
-  # Add randomly selected trajectories
-  # for(i in 1:ncol(s_trajs)){lines(x=s_ts$year, y=s_trajs[,i], col="grey80")}
-  polygon(x=c(s_ts$year, rev(s_ts$year)),
-          y=c(s_ts$q0.025, rev(s_ts$q0.975)), col="grey80", border=F)
-  # Add median and 95% CI trajectories
-  lines(x=s_ts$year, y=s_ts$q0.5, lwd=1.1)
-  # lines(x=s_ts$year, y=s_ts$q0.025, lwd=1.1, lty=2)
-  # lines(x=s_ts$year, y=s_ts$q0.975, lwd=1.1, lty=2)
-  # Add overfished line (saturation=0.25)
-  lines(x=c(xmin, xmax), y=c(0.5, 0.5), lty=2)
-  lines(x=c(xmin, xmax), y=c(0.25, 0.25), lty=3)
-  # Label end year saturation
-  text(x=yr_final, y=s_final, label=round(s_final,2), pos=4)
-
-  # E. B/BMSY time series
-  ##############################################################################
-
-  # Setup empty plot
-  xmin <- floor(min(bbmsy_ts$year) / 10) * 10
-  xmax <- ceiling(max(bbmsy_ts$year) / 10) * 10
-  plot(q0.5 ~ year, bbmsy_ts, type="n", bty="n", las=1,
+  xmin <- floor(min(ref_ts$year) / 10) * 10
+  xmax <- ceiling(max(ref_ts$year) / 10) * 10
+  plot(bbmsy ~ year, ref_ts, type="n", bty="n", las=1,
        xlim=c(xmin, xmax), ylim=c(0,2), xlab="", ylab=expression("B / B"["MSY"]),
-       main=expression(bold("E. B/B"["MSY"])))
+       main=expression(bold("C. B/B"["MSY"])))
   # Add randomly selected trajectories
-  # for(i in 1:ncol(bbmsy_trajs)){lines(x=bbmsy_ts$year, y=bbmsy_trajs[,i], col="grey80")}
-  polygon(x=c(bbmsy_ts$year, rev(bbmsy_ts$year)),
-          y=c(bbmsy_ts$q0.025, rev(bbmsy_ts$q0.975)), col="grey80", border=F)
+  # for(i in 1:ncol(bbmsy_trajs)){lines(x=ref_ts$year, y=bbmsy_trajs[,i], col="grey80")}
+  polygon(x=c(ref_ts$year, rev(ref_ts$year)),
+          y=c(ref_ts$bbmsy_lo, rev(ref_ts$bbmsy_hi)), col="grey80", border=F)
   # Add median and 95% CI trajectories
-  lines(x=bbmsy_ts$year, y=bbmsy_ts$q0.5, lwd=1.1)
-  # lines(x=bbmsy_ts$year, y=bbmsy_ts$q0.025, lwd=1.1, lty=3)
-  # lines(x=bbmsy_ts$year, y=bbmsy_ts$q0.975, lwd=1.1, lty=3)
+  lines(x=ref_ts$year, y=ref_ts$bbmsy, lwd=1.1)
   # Add overfished line (B/BMSY=0.5)
   lines(x=c(xmin, xmax), y=c(0.5, 0.5), lty=3)
   lines(x=c(xmin, xmax), y=c(1, 1), lty=2)
   # Label end year B/BMSY
-  text(x=yr_final, y=bbmsy_final, label=round(bbmsy_final,2), pos=4)
+  text(x=yr2, y=bbmsy_final, label=round(bbmsy_final,2), pos=4)
+
+  # D. F/FMSY time series
+  ##############################################################################
+
+  # Setup empty plot
+  xmin <- floor(min(ref_ts$year) / 10) * 10
+  xmax <- ceiling(max(ref_ts$year) / 10) * 10
+  ymax <- ceiling(max(ref_ts$ffmsy_hi) / 0.5) * 0.5
+  plot(ffmsy ~ year, ref_ts, type="n", bty="n", las=1,
+       xlim=c(xmin, xmax), ylim=c(0,ymax), xlab="", ylab=expression("F / F"["MSY"]),
+       main=expression(bold("D. F/F"["MSY"])))
+  # Add polygon and line
+  polygon(x=c(ref_ts$year, rev(ref_ts$year)),
+          y=c(ref_ts$ffmsy_lo, rev(ref_ts$ffmsy_hi)), col="grey70", border=F)
+  lines(x=ref_ts$year, y=ref_ts$ffmsy, lwd=1.1)
+  # Label end year F/FMSY
+  lines(x=c(xmin, xmax), y=c(1, 1), lty=2)
+  text(x=yr2, y=ffmsy_final, label=round(ffmsy_final,2), pos=4, xpd=NA)
+
+  # E. Kobe plot
+  ##############################################################################
+
+  # Setup plot
+  xmax <- ceiling(max(ref_ts$bbmsy) / 0.5) * 0.5
+  ymax <- ceiling(max(ref_ts$ffmsy) / 0.5) * 0.5
+  plot(ffmsy ~ bbmsy, ref_ts, bty="n", type="l", las=1,
+       xlim=c(0,xmax), ylim=c(0,ymax), main="E. Kobe plot",
+       xlab=expression("B / B"["MSY"]), ylab=expression("F / F"["MSY"]))
+  lines(x=c(0, xmax), y=c(1,1), lty=2)
+  lines(x=c(1, 1), y=c(0,ymax), lty=2)
+  # Add start/end points
+  points(x=ref_ts$bbmsy[1], y=ref_ts$ffmsy[1], pch=22, cex=1.4, bg="white")
+  points(x=ref_ts$bbmsy[nrow(ref_ts)], y=ref_ts$ffmsy[nrow(ref_ts)], pch=22, cex=1.4, bg="grey70")
+  # Add points legend
+  legend("topright", bty="n", legend=c(yr1, yr2), pch=22, pt.bg=c("white", "grey70"), pt.cex=1.4)
+
 
 }
